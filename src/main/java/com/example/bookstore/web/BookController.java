afa08850bc57b391a0e18728b6b612cbc07966f0
jarhodes <github@rhodes.fi>
2020-09-03 18:27:30 +0300

    Refer to template

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,5 +1,18 @@
 package com.example.bookstore.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BookController {
+	
+	@GetMapping("/index")
+	public String index(Model model) {
+		
+		model.addAttribute("title", "Bookstore");
+		
+		return "index";
+	}
 
 }
