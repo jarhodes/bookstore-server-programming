5ad106a177efb24dcdfc58402d507bff32ca2697
jarhodes <github@rhodes.fi>
2020-10-12 14:04:21 +0300

    Stop adding new database rows on startup

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -27,19 +27,20 @@ public class BookstoreApplication {
 	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository catRepository, UserRepository userRepository) {
 		return (args) -> {
 			log.info("Add a couple of categories");
+			/*
 			catRepository.save(new Category("Satire"));
 			catRepository.save(new Category("Absurdist"));
 			
 			log.info("Add a couple of books");
 			repository.save(new Book("Catch-22", 1961, "0-671-12805-1", 8.83, catRepository.findByName("Satire").get(0)));
 			repository.save(new Book("American Psycho", 1991, "978-0-679-73577-9", 7.38, catRepository.findByName("Absurdist").get(0)));
-			repository.save(new Book("1984", 1949, "9780141391700", 10.99, catRepository.findByName("Satire").get(0)));
+			repository.save(new Book("1984", 1949, "9780141391700", 10.99, catRepository.findByName("Satire").get(0)));*/
 			
 			log.info("Add a couple of users");
-			User user1 = new User("user", "$2y$12$cGC.nL381yrefl4tyqRaROoSU50TWdNSKBewhb00QiBWrv5ulRUjK", "user@example.com", "USER");
-			User user2 = new User("admin", "$2y$12$DYdPwijEOWp7mVCaQuqqp.D3ZkSidHjsqmuSONvDvktZsnz7yfAGi", "admin@example.com", "ADMIN");
-			userRepository.save(user1);
-			userRepository.save(user2);
+			//User user1 = new User("user", "$2y$12$cGC.nL381yrefl4tyqRaROoSU50TWdNSKBewhb00QiBWrv5ulRUjK", "user@example.com", "USER");
+			//User user2 = new User("admin", "$2y$12$DYdPwijEOWp7mVCaQuqqp.D3ZkSidHjsqmuSONvDvktZsnz7yfAGi", "admin@example.com", "ADMIN");
+			//userRepository.save(user1);
+			//userRepository.save(user2);
 			
 			log.info("Fetch all books");
 			for (Book book : repository.findAll()) {
